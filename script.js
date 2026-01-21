9c9be8bf114b4ab247d6bfd06b580e25142bdb7e
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2026-01-22 00:03:41 +0100

    added new modal for contact me in card

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownElementList = [].slice.call(
+  document.querySelectorAll(".dropdown-toggle"),
+);
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
-})
+  return new bootstrap.Dropdown(dropdownToggleEl);
+});
