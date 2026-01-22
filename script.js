f0f9d07004c723730d3f4d70dacffd698253bc6b
Sertzan-1080516 <146075366+Sertzan-1080516@users.noreply.github.com>
2026-01-22 12:41:09 +0100

    copy function added to copy mail adress,github link and linkedin link

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ var dropdownElementList = [].slice.call(
 var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl);
 });
+
+function copy(id) {
+  let copy = document.getElementById(id).href;
+  if (copy.startsWith("mailto:")) {
+    copy = copy.slice(7);
+  }
+  navigator.clipboard.writeText(copy);
+}
